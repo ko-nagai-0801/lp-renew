dc8756daffd5e32d9e2fd777fdb7557596c4478d
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-08-05 09:40:39 +0900

    Update: footer Update

--- footer.php
+++ footer.php
@@ -12,61 +12,73 @@
         <!-- Company block --------------------------------------------------- -->
         <address class="footer__company">
 
-            <div class="footer__logo">
-                <img src="<?php echo esc_url(get_theme_file_uri('img/logo-white.svg')); ?>" alt="">
+            <div class="footer__logo py-5">
+                <img src="<?php echo esc_url(get_theme_file_uri('assets/img/footer-logo.svg')); ?>" alt="">
             </div>
 
-            <p class="footer__address">
-                株式会社ＬｉＮＥ&nbsp;ＰＡＲＫ<br>
-                〒120-0005 東京都足立区綾瀬&nbsp;2-27-4&nbsp;D1&nbsp;AYASE&nbsp;2F
-            </p>
+            <div class="footer__company-item">
+                <p class="footer__address py-3">
+                    株式会社ＬｉＮＥ&nbsp;ＰＡＲＫ<br>
+                    〒120-0005<br>
+                    東京都足立区綾瀬&nbsp;2-27-4&nbsp;D1&nbsp;AYASE&nbsp;2F
+                </p>
+            </div>
 
-            <p class="footer__mail">
-                <img src="<?php echo esc_url(get_theme_file_uri('img/common/mail-icon.svg')); ?>" alt="">
-                <a href="mailto:info@linepark.co.jp" data-nosnippet>info@linepark.co.jp</a>
-            </p>
-
-            <p class="footer__phone">
-                <img src="<?php echo esc_url(get_theme_file_uri('img/common/phone-icon.svg')); ?>" alt="">
-                <a href="tel:03-4400-5584">03-4400-5584</a><br>
-                <span class="footer__time">受付：月-金&nbsp;9:00-17:00</span>
-            </p>
+            <div class="footer__company-item">
+                <p class="footer__mail">
+                    <i class="bi bi-send-fill"></i>
+                    <a href="mailto:info@linepark.co.jp" data-nosnippet>info@linepark.co.jp</a>
+                </p>
+                <p class="footer__phone">
+                    <i class="bi bi-telephone-fill"></i>
+                    <a href="tel:03-4400-5584">03-4400-5584</a><br>
+                <p class="footer__time">受付：月-金&nbsp;9:00-17:00（土・日・祝日は閉所）</p>
+                </p>
+            </div>
 
             <!-- SNS -->
-            <ul class="footer__sns">
-                <li class="footer__sns-item">
-                    <a href="https://www.instagram.com/linepark.ayase/" target="_blank" aria-label="Instagram">
-                        <img src="<?php echo esc_url(get_theme_file_uri('img/common/instagram-icon.svg')); ?>" alt="">
-                    </a>
-                </li>
-                <li class="footer__sns-item">
-                    <a href="https://twitter.com/LiNEPARK_ayase" target="_blank" aria-label="X">
-                        <img src="<?php echo esc_url(get_theme_file_uri('img/common/x-icon.webp')); ?>" alt="">
-                    </a>
-                </li>
-                <li class="footer__sns-item">
-                    <a href="https://www.youtube.com/@fusafusachannel" target="_blank" aria-label="YouTube">
-                        <img src="<?php echo esc_url(get_theme_file_uri('img/common/youtube-icon.svg')); ?>" alt="">
-                    </a>
-                </li>
-                <li class="footer__sns-item">
-                    <a href="https://www.tiktok.com/@linepark.inc" target="_blank" aria-label="TikTok">
-                        <img src="<?php echo esc_url(get_theme_file_uri('img/common/tictok-icon.svg')); ?>" alt="">
-                    </a>
-                </li>
-            </ul>
+            <div class="footer__company-item footer__company-item-sns">
+                <ul class="footer__sns-list">
+                    <li class="footer__sns-item">
+                        <a href="https://www.instagram.com/linepark.ayase/" target="_blank" aria-label="Instagram">
+                            <img src="<?php echo esc_url(get_theme_file_uri('assets/img/common/instagram-icon.svg')); ?>" alt="">
+                        </a>
+                    </li>
+                    <li class="footer__sns-item">
+                        <a href="https://twitter.com/LiNEPARK_ayase" target="_blank" aria-label="X">
+                            <img src="<?php echo esc_url(get_theme_file_uri('assets/img/common/x-icon.webp')); ?>" alt="">
+                        </a>
+                    </li>
+                    <li class="footer__sns-item">
+                        <a href="https://www.youtube.com/@fusafusachannel" target="_blank" aria-label="YouTube">
+                            <img src="<?php echo esc_url(get_theme_file_uri('assets/img/common/youtube-icon.svg')); ?>" alt="">
+                        </a>
+                    </li>
+                    <li class="footer__sns-item">
+                        <a href="https://www.tiktok.com/@linepark.inc" target="_blank" aria-label="TikTok">
+                            <img src="<?php echo esc_url(get_theme_file_uri('assets/img/common/tictok-icon.svg')); ?>" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
         </address>
 
         <!-- Footer-nav ------------------------------------------------------ -->
         <nav class="footer__nav">
             <ul class="footer__nav-list">
-                <li class="footer__nav-item"><a href="/about/">ＬｉＮＥ&nbsp;ＰＡＲＫ について</a></li>
-                <li class="footer__nav-item"><a href="/#business">事業内容</a></li>
-                <li class="footer__nav-item"><a href="/products-web/">┗ Web制作</a></li>
-                <li class="footer__nav-item"><a href="/products-movie/">┗ 動画編集</a></li>
-                <li class="footer__nav-item"><a href="/products-design/">┗ デザイン制作</a></li>
-                <li class="footer__nav-item"><a href="/access/">アクセスガイド</a></li>
+                <li class="footer__nav-item"><a href="/about/">ＬｉＮＥ&nbsp;ＰＡＲＫについて</a></li>
+                <li class="footer__nav-item"><a href="/#business">事業内容</a>
+                    <ul class="footer__service-nav-list">
+                        <li class="footer__service-nav-item"><a href="/products-web/">Web制作</a></li>
+                        <li class="footer__service-nav-item"><a href="/products-movie/">動画編集</a></li>
+                        <li class="footer__service-nav-item"><a href="/products-design/">デザイン制作</a></li>
+                        <li class="footer__service-nav-item"><a href="/products-design/">デザイン制作</a></li>
+                        <li class="footer__service-nav-item"><a href="/products-design/">デザイン制作</a></li>
+                        <li class="footer__service-nav-item"><a href="/products-design/">デザイン制作</a></li>
+                    </ul>
+                </li>
+                <li class="footer__nav-item"><a href="/access/">アクセス</a></li>
                 <li class="footer__nav-item"><a href="/#news">お知らせ</a></li>
                 <li class="footer__nav-item"><a href="/recruit/">採用情報</a></li>
                 <li class="footer__nav-item"><a href="/associates/">協力企業</a></li>
@@ -78,7 +90,7 @@
     </div><!-- /.footer__inner -->
 
     <small class="footer__copyright">
-        &copy;&nbsp;<?php echo date('Y'); ?>&nbsp;<?php bloginfo('name'); ?> Inc.
+        &copy;&nbsp;2022&nbsp;<?php bloginfo('name'); ?> Inc.
     </small>
 
 </footer>
